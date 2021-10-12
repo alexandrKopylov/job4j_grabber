b300bbb4c0ac55e7ebcf3c4d66efc0ec2836b841
alexandrKopylov <sash-2007@ya.ru>
2021-10-12 23:58:18 +0500

    2. Парсинг HTML страницы. [#260358]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -9,11 +9,17 @@ public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".postslisttopic");
+        String jobText = null;
+        String jobDate = null;
         for (Element td : row) {
             Element href = td.child(0);
-            // System.out.println(href.attr("href"));
-            if (href.text().contains("Java") ||  href.text().contains("java")) {
-                System.out.println(href.text());
+            jobText = href.text();
+            if (jobText.toLowerCase().contains("java")) {
+                Element parent = td.parent();
+                jobDate = parent.child(5).text();
+                System.out.println(jobText + " : " + jobDate);
+                System.out.println(href.attr("href"));
+                System.out.println();
             }
         }
     }
