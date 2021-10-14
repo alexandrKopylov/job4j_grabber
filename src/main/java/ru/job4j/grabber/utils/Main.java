b5074e0d6dca1393bf7f77599fdedc25c35f3c82
Sasha <sash-2007@ya.ru>
2021-10-14 11:40:51 +0500

    parser  DataTime -2

--- src/main/java/ru/job4j/grabber/utils/Main.java
+++ src/main/java/ru/job4j/grabber/utils/Main.java
@@ -22,30 +22,32 @@ public class Main {
 //        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 //        LocalDateTime dateTime = LocalDateTime.parse(str, formatter);
 //
-         final Map<String, String> MONTHS = Map.ofEntries(
-                entry("дек", "декабрь"),
-                entry("янв", "Jan"),
-                entry("фев", "февраль"),
-                entry("мар", "март"),
-                entry("апр", "апрель"),
-                entry("май", "май"),
-                entry("июн", "июнь"),
-                entry("июл", "июль"),
-                entry("авг", "август"),
-                entry("сен", "сентябрь"),
-                entry("окт", "октябрь"),
-                entry("ноя", "ноябрь")
-        );
-         String sss = "2 янв 2005 22:29";
-       // String sss = "02 08 2020 22:29";
-        String[] mas = sss.split(" ");
-       String rrr = MONTHS.get(mas[1]);
-           mas[1]  = MONTHS.get(mas[1]);
 
-        String name = String.join(" ", mas);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM yyyy HH:mm", Locale.ENGLISH);
-        LocalDateTime dateTime = LocalDateTime.parse(name, formatter);
-        System.out.println(dateTime);
+
+//         final Map<String, String> MONTHS = Map.ofEntries(
+//                entry("дек", "декабрь"),
+//                entry("янв", "Jan"),
+//                entry("фев", "февраль"),
+//                entry("мар", "март"),
+//                entry("апр", "апрель"),
+//                entry("май", "май"),
+//                entry("июн", "июнь"),
+//                entry("июл", "июль"),
+//                entry("авг", "август"),
+//                entry("сен", "сентябрь"),
+//                entry("окт", "октябрь"),
+//                entry("ноя", "ноябрь")
+//        );
+//         String sss = "2 янв 05 22:29";
+//       // String sss = "02 08 2020 22:29";
+//        String[] mas = sss.split(" ");
+//       String rrr = MONTHS.get(mas[1]);
+//           mas[1]  = MONTHS.get(mas[1]);
+//
+//        String name = String.join(" ", mas);
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM yyyy HH:mm", Locale.ENGLISH);
+//        LocalDateTime dateTime = LocalDateTime.parse(name, formatter);
+//        System.out.println(dateTime);
 
 //        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMMM-dd-yyyy");
 //        LocalDate date = LocalDate.parse("February-23-2019", dtf);
@@ -59,6 +61,20 @@ public class Main {
 //        String sss =  dateTime.format(formatter);
 //        System.out.println(sss);
       //  Locale.ENGLISH
+
+
+       // String sss = "5 дек 05, 22:29";
+
+         String sss = "вчера, 22:29";
+        SqlRuDateTimeParser rrrr = new SqlRuDateTimeParser();
+
+
+        LocalDateTime ddd = rrrr.parse(sss);
+        System.out.println(ddd);
+
+//        LocalDateTime ldt = LocalDateTime.now();
+//        System.out.println(ldt);
+//        System.out.println( ldt.minusDays(1));
 
     }
 
--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -1,36 +1,53 @@
 package ru.job4j.grabber.utils;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.Map;
 
 import static java.util.Map.entry;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
-
     private static final Map<String, String> MONTHS = Map.ofEntries(
-            entry("дек", "декабрь"),
-            entry("янв", "январь"),
-            entry("фев", "февраль"),
-            entry("мар", "март"),
-            entry("апр", "апрель"),
-            entry("май", "май"),
-            entry("июн", "июнь"),
-            entry("июл", "июль"),
-            entry("авг", "август"),
-            entry("сен", "сентябрь"),
-            entry("окт", "октябрь"),
-            entry("ноя", "ноябрь")
-            );
+            entry("дек", "Dec"),
+            entry("янв", "Jan"),
+            entry("фев", "Feb"),
+            entry("мар", "Mar"),
+            entry("апр", "Apr"),
+            entry("май", "May"),
+            entry("июн", "Jun"),
+            entry("июл", "Jul"),
+            entry("авг", "Aug"),
+            entry("сен", "Sep"),
+            entry("окт", "Oct"),
+            entry("ноя", "Nov")
+    );
 
     @Override
     public LocalDateTime parse(String parse) {
-
+        LocalDateTime dateTime = null;
         String[] mas = parse.split(" ");
-
-
-
-        return null;
+        if (mas[0].trim().length() == 1) {
+            mas[0] = "0" + mas[0];
+        }
+        String mapReturn = MONTHS.get(mas[1]);
+        if (mapReturn != null) {
+            mas[1] = mapReturn;
+            String name = String.join(" ", mas);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yy, HH:mm", Locale.ENGLISH);
+            dateTime = LocalDateTime.parse(name, formatter);
+        } else {
+            LocalDate ld = LocalDate.now();
+            String[] hourMinutes = mas[1].split(":");
+            LocalDateTime createDataTimes = LocalDateTime.of(ld.getYear(),
+                    ld.getMonth(),
+                    ld.getDayOfMonth(),
+                    Integer.parseInt(hourMinutes[0]),
+                    Integer.parseInt(hourMinutes[1])
+            );
+            dateTime = (mas[0].equals("сегодня,")) ? createDataTimes : createDataTimes.minusDays(1);
+        }
+        return dateTime;
     }
-
-
 }
