1fa5ea926f56fff9dd4700ed56d66d1d2631cf69
Sasha <sash-2007@ya.ru>
2021-10-14 15:34:50 +0500

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,19 +7,22 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        String jobText = null;
-        String jobDate = null;
-        for (Element td : row) {
-            Element href = td.child(0);
-            jobText = href.text();
-            if (jobText.toLowerCase().contains("java")) {
-                Element parent = td.parent();
-                jobDate = parent.child(5).text();
-                System.out.println(jobText + " : " + jobDate);
-                System.out.println(href.attr("href"));
-                System.out.println();
+
+        for (int i = 1; i < 6; i++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+            Elements row = doc.select(".postslisttopic");
+            String jobText = null;
+            String jobDate = null;
+            for (Element td : row) {
+                Element href = td.child(0);
+                jobText = href.text();
+                if (jobText.toLowerCase().contains("java")) {
+                    Element parent = td.parent();
+                    jobDate = parent.child(5).text();
+                    System.out.println(jobText + " : " + jobDate);
+                    System.out.println(href.attr("href"));
+                    System.out.println();
+                }
             }
         }
     }
