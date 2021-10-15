f225d42d9d414e57b52bc596914ea1f88b0ff802
Sasha <sash-2007@ya.ru>
2021-10-15 11:34:11 +0500

    2.3. Загрузка деталей поста. [#285212 #216180]-2

--- src/main/java/ru/job4j/grabber/utils/DetailsPost.java
+++ src/main/java/ru/job4j/grabber/utils/DetailsPost.java
@@ -2,7 +2,6 @@ package ru.job4j.grabber.utils;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -11,18 +10,16 @@ public class DetailsPost {
 
     public void getDetals(String url) throws IOException {
         Document doc = Jsoup.connect(url).get();
-        Elements descriptionJob = doc.select("#content-wrapper-forum > table:nth-child(3) > tbody > tr:nth-child(2) > td:nth-child(2)");
-        System.out.println(descriptionJob.text());
-        Elements timeCreatedDescription = doc.select("#content-wrapper-forum > table:nth-child(3) > tbody > tr:nth-child(3) > td");
-        String str = timeCreatedDescription.text();
-        str = str.substring(0, 16);
-        System.out.println(str);
+        Elements descriptionJob = doc.select(".msgBody");
+        System.out.println(descriptionJob.get(1).text());
+        Elements selectAllFooter = doc.select(".msgFooter");
+        String descriptionTime = selectAllFooter.get(0).text();
+        System.out.println(descriptionTime.substring(0, 16));
     }
 
     public static void main(String[] args) throws IOException {
         String url = "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t";
         DetailsPost dp = new DetailsPost();
         dp.getDetals(url);
-
     }
 }
