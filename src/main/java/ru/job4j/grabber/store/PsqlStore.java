7ebb2fa3b0a49cdea7614b9034074ec7b22bdf8d
Sasha <sash-2007@ya.ru>
2021-10-19 11:05:23 +0500

    5. PsqlStore [#285209 #217844]-2

--- src/main/java/ru/job4j/grabber/store/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/store/PsqlStore.java
@@ -1,13 +1,15 @@
 package ru.job4j.grabber.store;
 
 import ru.job4j.grabber.model.Post;
+import ru.job4j.grabber.parse.SqlRuParse;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 import ru.job4j.quartz.AlertRabbit;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 import java.util.Properties;
 
@@ -33,16 +35,22 @@ public class PsqlStore implements Store, AutoCloseable {
         }
     }
 
+
     @Override
     public void save(Post post) {
         try (PreparedStatement statement =
-                     cnn.prepareStatement("insert into post(name, text, link, created) values (?,?,?,?)")) {
+                     cnn.prepareStatement("insert into post(name, text, link, created) values (?,?,?,?)",
+                             Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, post.getTitle());
             statement.setString(2, post.getDescription());
             statement.setString(3, post.getLink());
             statement.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
             statement.execute();
-
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    post.setId(generatedKeys.getInt(1));
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -96,5 +104,30 @@ public class PsqlStore implements Store, AutoCloseable {
         if (cnn != null) {
             cnn.close();
         }
+    }
+
+    public static Properties getProperty() {
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            return config;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Properties prop = getProperty();
+        PsqlStore psqlStore = new PsqlStore(prop);
+        SqlRuParse parse = new SqlRuParse(new SqlRuDateTimeParser());
+        Post post1 = parse.detail("https://www.sql.ru/forum/1339483/vakansiya-oracle-razrabotchik-ot-170-k-udalenka");
+        post1.setCreated(LocalDateTime.now());
+        Post post2 = parse.detail("https://www.sql.ru/forum/1339524/senior-middle-dba-postgresql-ot-dataegret-udalyonnaya-rabota");
+        post2.setCreated(LocalDateTime.now());
+        psqlStore.save(post1);
+        psqlStore.save(post2);
+        psqlStore.getAll().forEach(System.out::println);
+        System.out.println("-----------------------------------------------");
+        System.out.println(psqlStore.findById(1));
     }
 }
